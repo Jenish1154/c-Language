641f156e4e427e5be736edc35c67d1bccaa1fba9
Jenish1154 <134709254+Jenish1154@users.noreply.github.com>
2023-07-07 12:40:42 +0530

    Add files via upload

--- jenish day 27/task 1.cpp
+++ jenish day 27/task 1.cpp
@@ -1,26 +1,78 @@
 #include<stdio.h>
-void swap(int *n1,int *n2){
-    int n3;
-	n3=*n1;
-	*n1=*n2;
-	*n2=n3;
-	printf(" A : %d\n B : %d\n",*n1,*n2);
+#include<string.h>
+
+struct Student{
+	int id;
+	char name[20];
+	int age;
+	char course[20];
+	char school[20];
+	char city[20];
+    int std;
 	
-	
-}
+};
 int main(){
-	int a,b;
-	printf("Enter A : ");
-	scanf("%d",&a);
-	
-	printf("Enter B : ");
-	scanf("%d",&b);
-		printf("\n");
-	
-	swap(&a,&b);
-	
+  int n;
+  
+  printf("Enter Student Number   :");
+  scanf("%d",&n);	
+  printf("\n");
+  
+  struct Student stu[n];
+  
+  int i;
+    for(i=0;i<n;i++){
+    	
+		printf("Enter Student [%d] Id : ",i);
+		
+		scanf("%d",&stu[i].id);
+    	printf("\n");
+    	
+    	printf("Enter Student [%d] Name : ",i);
+    	scanf("%s",stu[i].name);
+    	printf("\n");
+    	
+		printf("Enter Student [%d] Course : ",i);
+    	scanf("%s",stu[i].course);
+    	printf("\n");
+    	 
+    	printf("Enter Student [%d] Std : ",i);
+    	scanf("%d",&stu[i].std); 
+    	printf("\n");
+    	
+    	printf("Enter Student [%d] Age : ",i);
+    	scanf("%d",&stu[i].age);
+    	printf("\n");
+		 
+    	printf("Enter Student [%d] City : ",i);
+    	scanf("%s",stu[i].city);
+    	printf("\n");
+    	
+	}
+	 for(i=0;i<n;i++){
+	 	
+        printf("Student [%d]..  \n",i+1);
+    	printf("Id      :   %d",stu[i].id);
+        printf("\n");
+    
+    	printf("Name    :   %s",stu[i].name);
+        printf("\n");	
+    
+    	printf("Course  :   %s",stu[i].course);
+    	printf("\n");	
+    
+    	printf("Standard:   %d",stu[i].std); 
+    	printf("\n");
+    	 
+    	printf("Age     :   %d",stu[i].age);
+        printf("\n");	
+    
+    	printf("City    :   %s",stu[i].city);
+        printf("\n\n");	
+    	
+	}
 	
 	
 	
 	return 0;
-}
+} 
--- jenish day 27/task 2.cpp
+++ jenish day 27/task 2.cpp
@@ -1,31 +1,88 @@
 #include<stdio.h>
+#include<string.h>
+
+struct Employee{
+	
+	int id;
+	char name[20];
+	int age;
+	char role[20];
+	char company_name[20];
+	char city[20];
+    int expirence;
+	
+};
+
 int main(){
 
-int i,n;
+  int n;
 
-    printf("Enter Size : ");
-    scanf("%d",&n);
-
-    int arr[n];
+  printf("Enter Employee Number   :");
+  scanf("%d",&n);	
+  printf("\n");
+  
+  struct Employee emp[n];
+  
+  int i;
+    for(i=0;i<n;i++){
+  
+    	printf("Enter Employee [%d] Id : ",i+1);
+    	scanf("%d",&emp[i].id);
+    	printf("\n");
+    	
+    	printf("Enter Employee [%d] Name : ",i+1);
+    	scanf("%s",emp[i].name);
+    	printf("\n");
+    	
+    	printf("Enter Employee [%d] Role : ",i+1);
+    	scanf("%s",emp[i].role);
+    	printf("\n");
+    	
+		printf("Enter Employee [%d] Expirence : ",i+1);
+    	scanf("%d",&emp[i].expirence); 
+    	printf("\n");
+    	
+		printf("Enter Employee [%d] Age : ",i+1);
+    	scanf("%d",&emp[i].age);
+    	printf("\n");
+    	
+		printf("Enter Employee [%d] City : ",i+1);
+    	scanf("%s",emp[i].city);
+    	printf("\n");
+    	
+		printf("Enter Employee [%d] Company Name : ",i+1);
+    	scanf("%s",emp[i].company_name);
+    	printf("\n");
+    	
+		   	
+	}
+	 for(i=0;i<n;i++){
+     
+	 printf("\nEmployee [%d].....  \n",i+1);
+     printf("\n");
+     
+	 printf("Id              :   %d",emp[i].id);
+     printf("\n");
+    	
+     printf("Name            :   %s",emp[i].name);
+     printf("\n");	
     
-	for(i=0;i<n;i++){
-    	printf("Enter Element [%d] : ",i);
-    	scanf("%d",&arr[i]);
+     printf("Role            :   %s",emp[i].role);
+     printf("\n");	
+    
+     printf("Expirence       :   %d",emp[i].expirence); 
+     printf("\n");
+    	
+	 printf("Age             :   %d",emp[i].age);
+     printf("\n");	
+    
+     printf("City            :   %s",emp[i].city);
+     printf("\n");	
+    
+     printf("Company Name    :   %s",emp[i].company_name);
+     printf("\n\n");	
     	
 	}
-	int *a[n];
 	
-     for(i=0;i<n;i++){
-     a[i]=&arr[i];
-	}
-	
-	printf("\n");
-	for(i=0;i<n;i++){
-    	printf("%d ",*a[i]);
-     	
-	}
-    
-    
-    return 0;
-}
-   
+	return 0;
+} 
