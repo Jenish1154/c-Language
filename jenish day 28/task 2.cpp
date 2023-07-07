3581a4a2f37762609a2fe9cc7ec3fdb392ae5674
Jenish1154 <134709254+Jenish1154@users.noreply.github.com>
2023-07-07 12:48:54 +0530

    Add files via upload

---
+++ jenish day 28/task 2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	
+	FILE *p;
+	char name[32];
+	
+	p = fopen("docs.txt","r");
+	
+	if(p==NULL){
+		printf("Error");
+		
+	}
+	else{
+		
+	    fgets(name,20,p);
+	    puts(name);
+	
+		fclose(p);
+		
+	}
+	
+	return 0;
+}
